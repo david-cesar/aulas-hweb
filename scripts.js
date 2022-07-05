6eeaa422925e3d3abf2b32e469ec52bb42608c84
David Silva <silva.davidcesar@gmail.com>
2022-07-05 20:33:21 -0300

    adicionamos o arquivo de anotações, estilos e scripts

---
+++ scripts.js
@@ -0,0 +1,23 @@
+console.log("QuerySelector do H1:");
+console.log(document.querySelector("h1"));
+
+console.log("QuerySelectorAll dos elementos p");
+console.log(document.querySelectorAll("p"));
+
+console.log("Selecionando a partir da classe:");
+console.log(document.getElementsByClassName("titulodapagina"));
+
+console.log("Selecionando a partir do ID:");
+console.log(document.getElementById("subtitulomeio"));
+
+console.log("Selecionando a partir da CLASSE USANDO QuerySelector");
+console.log(document.querySelector(".titulodapagina"));
+
+console.log("Selecionando a partir ");
+console.log(document.querySelector(".titulodapagina"));
+
+let variavelA = 10;
+
+let paragrafos = document.querySelectorAll("p");
+console.log("Paragrafos: ")
+console.log(paragrafos);
